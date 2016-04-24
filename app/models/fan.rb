60eff6494948581b188763b115bd89b412803c7b
clberry7107 <clberry7107@yahoo.com>
2016-04-24 15:26:11 +0000

    fix for fan stuff

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -3,5 +3,9 @@ class Fan < ActiveRecord::Base
   validates :email, uniqueness: true
   validates :f_name, :l_name, :email, presence: true
   
+  def full_name
+    return "#{self.f_name} #{self.l_name}"
+  end
+  
 end
   
