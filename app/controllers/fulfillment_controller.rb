ff32b823540af7c37a1558a0496bda68f172519a
clberry7107 <clberry7107@yahoo.com>
2016-09-09 20:26:09 +0000

    changed layout, added search functionality

--- app/controllers/fulfillment_controller.rb
+++ app/controllers/fulfillment_controller.rb
@@ -2,18 +2,21 @@ class FulfillmentController < ApplicationController
   before_action :authenticate_user!
    
   def index
+    if params[:q]
+      params[:format] = params[:q][:format_eq]
+    end
     case params[:format]
       when "subscriber"
-        @subscribers = Array.new
-        Subscriber.order(:last_name).all.each do |subscriber|
-          @subscribers << subscriber unless !subscriber.active || subscriber.active_subscriptions == 0 
-        end
+        @subscribers = Subscriber.includes(:subscribers_books).where(active: [true,:true], subscribers_books: { quantity: 1}).order(:last_name)
+        @search = @subscribers.search(params[:q])
+        @subscribers = @search.result
+        # @subscribers = @subscribers.paginate(:page => params[:page], :per_page => 5)
         @format = "subscriber"
       when "book"
-        @books = Array.new
-        Book.order(:title).all.each do |book|
-          @books << book unless !book.active || book.subscribers.count == 0
-        end
+        @books = Book.includes(:subscribers_books).where(active: [true,:true], subscribers_books: { quantity: 1}).order(:title)
+        @search = @books.search(params[:q])
+        @books = @search.result
+        # @books = @books.paginate(:page => params[:page], :per_page => 5)
         @format = "book"
       else
         @format = "none"
