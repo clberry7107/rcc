93c917a75c7aa2f707b501497256d5a0c3b930df
clberry7107 <clberry7107@yahoo.com>
2016-10-05 05:02:17 +0000

    fix for devise email in production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://rubber-city-comics.com'}
+  config.action_mailer.default_url_options = { host: 'http://rubber-city-comics.herokuapp.com/'}
   
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
